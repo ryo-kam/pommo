6aa7df7e4c5687c41f205dc644d9bd4db017c62f
Ryo Kamata <rkam1058@gmail.com>
2026-03-10 14:25:57 +1300

    Base terminal interface

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,100 @@
-use ratatui::{DefaultTerminal, Frame};
+use std::{io, time::Duration};
+
+use crossterm::event::{self, Event, KeyCode, KeyEventKind};
+use ratatui::{
+    DefaultTerminal, Frame,
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+};
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
-    ratatui::run(app)?;
+    ratatui::run(|terminal| App::default().run(terminal))?;
     Ok(())
 }
 
-fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
-    loop {
-        terminal.draw(render)?;
-        if crossterm::event::read()?.is_key_press() {
-            break Ok(());
+#[derive(Debug, Default)]
+pub struct App {
+    duration: Duration,
+    time_elapsed: Duration,
+    exit: bool,
+}
+
+impl App {
+    fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
         }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            // it's important to check that the event is a key press event as
+            // crossterm also emits key release and repeat events on Windows.
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: event::KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            _ => {}
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
     }
 }
 
-fn render(frame: &mut Frame) {
-    frame.render_widget("hello world", frame.area());
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer)
+    where
+        Self: Sized,
+    {
+        let title = Line::from("pommo").bold();
+
+        let instructions = Line::from(vec![
+            " Start ".into(),
+            "<R>".blue().bold(),
+            " Pause ".into(),
+            "<T>".red().bold(),
+            " Quit ".into(),
+            "<Q>".gray().bold(),
+            " ".into(),
+        ]);
+
+        let block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(instructions)
+            .border_set(border::THICK);
+
+        let time_left = self.duration.abs_diff(self.time_elapsed).as_secs();
+        let time_left_mins = time_left / 60;
+        let time_left_secs = time_left % 60;
+
+        let main_text = Text::from(vec![Line::from(vec![
+            "Timer: ".into(),
+            format!("{time_left_mins:0<2}:{time_left_secs:0<2}").yellow(),
+        ])]);
+
+        Paragraph::new(main_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
+    }
 }
